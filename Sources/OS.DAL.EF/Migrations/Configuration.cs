3a771dbc142ebe9bf06f211c1d9bd2e270c8ab78
agrynco <agrynco@gmail.com>
2015-08-28 10:08:40 +0300

    OS-29 Enable-migrations error

---
+++ Sources/OS.DAL.EF/Migrations/Configuration.cs
@@ -0,0 +1,30 @@
+#region Usings
+using System.Data.Entity.Migrations;
+#endregion
+
+namespace OS.DAL.EF.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<OnlineStoreDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(OnlineStoreDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
---
+++ Sources/OS.DAL.EF/OnlineStoreDbContext.cs
@@ -0,0 +1,20 @@
+﻿#region Usings
+using System.Data.Entity;
+using OS.Business.Domain;
+#endregion
+
+namespace OS.DAL.EF
+{
+    public class OnlineStoreDbContext : DbContext
+    {
+        public OnlineStoreDbContext() : this("OnlineStore")
+        {
+        }
+
+        public OnlineStoreDbContext(string nameOrConnectionString) : base(nameOrConnectionString)
+        {
+        }
+
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
