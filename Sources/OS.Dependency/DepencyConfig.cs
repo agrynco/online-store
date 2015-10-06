0bade205eedc68664305b1cf816af3e46245715a
Anatolii Grynchuk <agrynco@gmail.com>
2015-10-06 16:04:00 +0300

    Черговий раз привів все до єдиної версії .NET. Також виніс конфігурацію Dependency в окремий проект.

---
+++ Sources/OS.Dependency/DepencyConfig.cs
@@ -0,0 +1,20 @@
+﻿using Ninject;
+using OS.Business.Logic;
+using OS.Configuration;
+using OS.DAL.Abstract;
+using OS.DAL.EF;
+
+namespace OS.Dependency
+{
+    public static class DepencyConfig
+    {
+        public static void Configure(IKernel kernel)
+        {
+            kernel.Bind<EntityFrameworkDbContext>().ToSelf().WithConstructorArgument("nameOrConnectionString",
+            ApplicationSettings.Instance.DbSettings.ApplicationConnectionString);
+            kernel.Bind<IProductCategoriesRepository>().To<ProductCategoriesRepository>();
+            kernel.Bind<IOnlineStoreDbContext>().To<OnlineStoreDbContext>();
+            kernel.Bind<ProductCategoriesBL>().ToSelf();
+        }
+    }
+}
--- Sources/OS.Web/App_Start/NinjectWebCommon.cs
+++ Sources/OS.Web/App_Start/NinjectWebCommon.cs
@@ -1,36 +1,31 @@
-#region Usings
-using System;
-using System.Web;
-using Microsoft.Web.Infrastructure.DynamicModuleHelper;
-using Ninject;
-using Ninject.Web.Common;
-using OS.Business.Logic;
-using OS.Configuration;
-using OS.DAL.Abstract;
-using OS.DAL.EF;
-using OS.Web.App_Start;
-using WebActivatorEx;
-#endregion
-
-[assembly: WebActivatorEx.PreApplicationStartMethod(typeof (NinjectWebCommon), "Start")]
-[assembly: ApplicationShutdownMethod(typeof (NinjectWebCommon), "Stop")]
+using OS.Dependency;
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(OS.Web.App_Start.NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(OS.Web.App_Start.NinjectWebCommon), "Stop")]
 
 namespace OS.Web.App_Start
 {
-    public static class NinjectWebCommon
+    using System;
+    using System.Web;
+
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+    using Ninject;
+    using Ninject.Web.Common;
+
+    public static class NinjectWebCommon 
     {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
 
         /// <summary>
         /// Starts the application
         /// </summary>
-        public static void Start()
+        public static void Start() 
         {
-            DynamicModuleUtility.RegisterModule(typeof (OnePerRequestHttpModule));
-            DynamicModuleUtility.RegisterModule(typeof (NinjectHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
             bootstrapper.Initialize(CreateKernel);
         }
-
+        
         /// <summary>
         /// Stops the application.
         /// </summary>
@@ -38,7 +33,7 @@ namespace OS.Web.App_Start
         {
             bootstrapper.ShutDown();
         }
-
+        
         /// <summary>
         /// Creates the kernel that will manage your application.
         /// </summary>
@@ -48,7 +43,7 @@ namespace OS.Web.App_Start
             var kernel = new StandardKernel();
             try
             {
-                kernel.Bind<Func<IKernel>>().ToMethod(context => () => new Bootstrapper().Kernel);
+                kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
                 kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
 
                 RegisterServices(kernel);
@@ -67,11 +62,7 @@ namespace OS.Web.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
-            kernel.Bind<EntityFrameworkDbContext>().ToSelf().WithConstructorArgument("nameOrConnectionString",
-                ApplicationSettings.Instance.DbSettings.ApplicationConnectionString);
-            kernel.Bind<IProductCategoriesRepository>().To<ProductCategoriesRepository>();
-            kernel.Bind<IOnlineStoreDbContext>().To<OnlineStoreDbContext>();
-            kernel.Bind<ProductCategoriesBL>().ToSelf();
+            DepencyConfig.Configure(kernel);
         }
     }
 }
