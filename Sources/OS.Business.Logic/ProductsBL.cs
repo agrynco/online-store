0a873e230f3a9aeae3876d86fc5f2dcd15bb4878
Anatolii Grynchuk <agrynco@gmail.com>
2015-12-07 15:54:32 +0200

    OS-88 Розширити бізнеслогіку (OS-84) методом додавання нового товару в категорію

--- Sources/OS.Business.Logic/ProductsBL.cs
+++ Sources/OS.Business.Logic/ProductsBL.cs
@@ -1,3 +1,4 @@
+using OS.Business.Domain;
 using OS.DAL.Abstract;
 
 namespace OS.Business.Logic
@@ -5,10 +6,18 @@ namespace OS.Business.Logic
     public class ProductsBL
     {
         private readonly IProductsRepository _productsRepository;
+        private readonly IProductCategoriesRepository _productCategoriesRepository;
 
-        public ProductsBL(IProductsRepository productsRepository)
+        public ProductsBL(IProductsRepository productsRepository, 
+            IProductCategoriesRepository productCategoriesRepository)
         {
             _productsRepository = productsRepository;
+            _productCategoriesRepository = productCategoriesRepository;
+        }
+
+        public void Create(Product product)
+        {
+            _productsRepository.Add(product);
         }
     }
 }
