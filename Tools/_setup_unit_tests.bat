a077c88b4c257cc9ebfa5b0d35d0efe1af138151
Anatolii Grynchuk <agrynco@gmail.com>
2015-12-18 12:37:34 +0200

    Add force overwrite on add reg data

--- Tools/_setup_unit_tests.bat
+++ Tools/_setup_unit_tests.bat
@@ -1,4 +1,4 @@
 CALL _prepare-unit-tests_dbupdatesapplier.bat
 
-REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\OnlineStore.Tests\ConnectionStrings /v OnlineStore /t REG_SZ /d "Data Source=OnlineStoreDbSrv;Initial Catalog=DevOnlineStore_Tests;Persist Security Info=True;User ID=sa;Password=sa;"
-REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\OnlineStore.Tests\ConnectionStrings /v OnlineStore /t REG_SZ /d "Data Source=OnlineStoreDbSrv;Initial Catalog=DevOnlineStore_Tests;Persist Security Info=True;User ID=sa;Password=sa;"
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\OnlineStore.Tests\ConnectionStrings /v OnlineStore /t REG_SZ /d "Data Source=OnlineStoreDbSrv;Initial Catalog=DevOnlineStore_Tests;Persist Security Info=True;User ID=sa;Password=sa;" /f
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\OnlineStore.Tests\ConnectionStrings /v OnlineStore /t REG_SZ /d "Data Source=OnlineStoreDbSrv;Initial Catalog=DevOnlineStore_Tests;Persist Security Info=True;User ID=sa;Password=sa;" /f
--- Tools/prepare_dev_env.bat
+++ Tools/prepare_dev_env.bat
@@ -8,10 +8,10 @@ CD "%currentDir%"
 
 CALL setup_site.bat %domainName% %sitePath%
 
-REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\OnlineStore\ConnectionStrings /v OnlineStore /t REG_SZ /d "Data Source=OnlineStoreDbSrv;Initial Catalog=DevOnlineStore;Persist Security Info=True;User ID=sa;Password=sa;"
-REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\OnlineStore\ConnectionStrings /v OnlineStore /t REG_SZ /d "Data Source=OnlineStoreDbSrv;Initial Catalog=DevOnlineStore;Persist Security Info=True;User ID=sa;Password=sa;"
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\OnlineStore\ConnectionStrings /v OnlineStore /t REG_SZ /d "Data Source=OnlineStoreDbSrv;Initial Catalog=DevOnlineStore;Persist Security Info=True;User ID=sa;Password=sa;" /f
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\OnlineStore\ConnectionStrings /v OnlineStore /t REG_SZ /d "Data Source=OnlineStoreDbSrv;Initial Catalog=DevOnlineStore;Persist Security Info=True;User ID=sa;Password=sa;" /f
 
-REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\OnlineStore\AppSettings /v ApplicationEnvironment /t REG_SZ /d "Development"
-REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\OnlineStore\AppSettings /v ApplicationEnvironment /t REG_SZ /d "Development"
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\OnlineStore\AppSettings /v ApplicationEnvironment /t REG_SZ /d "Development" /f
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\OnlineStore\AppSettings /v ApplicationEnvironment /t REG_SZ /d "Development" /f
 
 CALL _setup_unit_tests.bat
