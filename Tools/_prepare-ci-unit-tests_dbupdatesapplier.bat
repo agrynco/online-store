5e7b277794f0fdbd356530e39ef3a972dbd6f6da
Anatolii Grynchuk <agrynco@gmail.com>
2015-12-18 13:09:34 +0200

    Unable to update database to match the current model because there are pending changes ...

---
+++ Tools/_prepare-ci-unit-tests_dbupdatesapplier.bat
@@ -0,0 +1,5 @@
+@SET DbUpdatesApplierFolder=%CD%\DbUpdatesApplier
+@SET DbUpdatesApplier="%DbUpdatesApplierFolder%\Tests_DbUpdateApplier.Console.exe"
+
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\OnlineStore.Tests\AppSettings /v dbUpdatesApplierExeName /t REG_SZ /d %DbUpdatesApplier% /f
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\OnlineStore.Tests\AppSettings /v dbUpdatesApplierExeName /t REG_SZ /d %DbUpdatesApplier% /f
