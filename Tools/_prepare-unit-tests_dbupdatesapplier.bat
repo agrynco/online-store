7fc00524b38cd1b04374554a659b2db7287ef77a
Anatolii Grynchuk <agrynco@gmail.com>
2015-12-18 12:10:31 +0200

    unit testing routine

---
+++ Tools/_prepare-unit-tests_dbupdatesapplier.bat
@@ -0,0 +1,5 @@
+@SET DbUpdatesApplierFolder=%CD%\DbUpdatesApplier
+@ECHO DbUpdatesApplier="%DbUpdatesApplierFolder%\Tests_DbUpdateApplier.Console.exe"
+
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\OnlineStore.Tests\AppSettings /v dbUpdatesApplierExeName /t REG_SZ /d %DbUpdatesApplier%
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\OnlineStore.Tests\AppSettings /v dbUpdatesApplierExeName /t REG_SZ /d %DbUpdatesApplier%
--- Tools/prepare_dev_env.bat
+++ Tools/prepare_dev_env.bat
@@ -10,3 +10,8 @@ CALL setup_site.bat %domainName% %sitePath%
 
 REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\OnlineStore\ConnectionStrings /v OnlineStore /t REG_SZ /d "Data Source=OnlineStoreDbSrv;Initial Catalog=DevOnlineStore;Persist Security Info=True;User ID=sa;Password=sa;"
 REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\OnlineStore\ConnectionStrings /v OnlineStore /t REG_SZ /d "Data Source=OnlineStoreDbSrv;Initial Catalog=DevOnlineStore;Persist Security Info=True;User ID=sa;Password=sa;"
+
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\OnlineStore\AppSettings /v ApplicationEnvironment /t REG_SZ /d "Development"
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\OnlineStore\AppSettings /v ApplicationEnvironment /t REG_SZ /d "Development"
+
+CALL _setup_unit_tests.bat
