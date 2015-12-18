1bbeaeb9aaceb0db4c4ceb196257fcdee9bf13fc
Anatolii Grynchuk <agrynco@gmail.com>
2015-12-18 14:37:47 +0200

    Try to fix creating registry keys in not x64 branch

--- Tools/prepare_ci_env.bat
+++ Tools/prepare_ci_env.bat
@@ -1,4 +1,9 @@
-CALL _prepare-ci-unit-tests_dbupdatesapplier.bat
+@SET DbUpdatesApplierFolder=%CD%\DbUpdatesApplier
+@SET DbUpdatesApplier="%DbUpdatesApplierFolder%\CI_Tests_DbUpdateApplier.Console.exe"
+
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\OnlineStore.Tests\AppSettings /v dbUpdatesApplierExeName /t REG_SZ /d %DbUpdatesApplier% /f
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\OnlineStore.Tests\AppSettings /v dbUpdatesApplierExeName /t REG_SZ /d %DbUpdatesApplier% /f
+
 
 REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\OnlineStore.Tests\ConnectionStrings /v OnlineStore /t REG_SZ /d "Data Source=OnlineStoreDbSrv;Initial Catalog=CiOnlineStore_Tests;Persist Security Info=True;User ID=sa;Password=sa;" /f
 REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\OnlineStore.Tests\ConnectionStrings /v OnlineStore /t REG_SZ /d "Data Source=OnlineStoreDbSrv;Initial Catalog=CiOnlineStore_Tests;Persist Security Info=True;User ID=sa;Password=sa;" /f
