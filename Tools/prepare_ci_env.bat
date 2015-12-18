e8263ea110655ddf02e31dc8bd4f90fb6c2279a2
Anatolii Grynchuk <agrynco@gmail.com>
2015-12-18 14:40:11 +0200

    Fucken magic

--- Tools/prepare_ci_env.bat
+++ Tools/prepare_ci_env.bat
@@ -2,6 +2,7 @@
 @SET DbUpdatesApplier="%DbUpdatesApplierFolder%\CI_Tests_DbUpdateApplier.Console.exe"
 
 REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\OnlineStore.Tests\AppSettings /v dbUpdatesApplierExeName /t REG_SZ /d %DbUpdatesApplier% /f
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\OnlineStore.Tests\AppSettings /v dbUpdatesApplierExeName /t REG_SZ /d %DbUpdatesApplier% /f
 REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\OnlineStore.Tests\AppSettings /v dbUpdatesApplierExeName /t REG_SZ /d %DbUpdatesApplier% /f
 
 
