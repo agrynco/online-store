fa876fd41a121c876da4b5fcbb0fdd545f9b4f10
agrynco <agrynco@gmail.com>
2016-01-01 15:32:46 +0200

    OS-128 Створити сторінку для перегляду потенційним покупцем деталей товару

--- Sources/OS.Web/Controllers/HomeController.cs
+++ Sources/OS.Web/Controllers/HomeController.cs
@@ -1,55 +1,12 @@
-﻿#region Usings
-using System.Linq;
-using System.Web.Mvc;
-using OS.Business.Domain;
-using OS.Business.Logic;
-using OS.Web.Models;
-#endregion
+﻿using System.Web.Mvc;
 
 namespace OS.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ProductCategoriesBL _productCategoriesBL;
-        private readonly ProductsBL _productsBL;
-
-        public HomeController(ProductCategoriesBL productCategoriesBL, ProductsBL productsBL)
+        public ActionResult Index()
         {
-            _productCategoriesBL = productCategoriesBL;
-            _productsBL = productsBL;
-        }
-
-        public virtual ActionResult Index()
-        {
-            HomePageViewModel viewModel = new HomePageViewModel();
-
-            viewModel.RootCategories = _productCategoriesBL.GetCategories(null).Select(productCategory => new HorizontalCategoryItemViewModel
-                {
-                    ProductCategory = productCategory,
-                    IsSelected = false
-                }).ToList();
-
-            viewModel.Products = _productsBL.Get(null);
-
-            return View(viewModel);
-        }
-
-        public ActionResult ChangeCategory(int categoryId)
-        {
-            HomePageViewModel viewModel = new HomePageViewModel();
-            viewModel.RootCategories = _productCategoriesBL.GetCategories(null).Select(productCategory => new HorizontalCategoryItemViewModel
-                {
-                    ProductCategory = productCategory,
-                    IsSelected = productCategory.Id == categoryId
-                }).ToList();
-
-            viewModel.SelectedCategory = viewModel.RootCategories.Single(productCategory => productCategory.ProductCategory.Id == categoryId).ProductCategory;
-            viewModel.Products = _productsBL.Get(new ProductsFilter
-                {
-                    ParentId = categoryId
-                });
-
-            return View("Index", viewModel);
+            return RedirectToAction("Index", "Products");
         }
     }
 }
---
+++ Sources/OS.Web/Controllers/ProductsController.cs
@@ -0,0 +1,60 @@
+﻿#region Usings
+using System.Linq;
+using System.Web.Mvc;
+using OS.Business.Domain;
+using OS.Business.Logic;
+using OS.Web.Models;
+#endregion
+
+namespace OS.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ProductCategoriesBL _productCategoriesBL;
+        private readonly ProductsBL _productsBL;
+
+        public ProductsController(ProductCategoriesBL productCategoriesBL, ProductsBL productsBL)
+        {
+            _productCategoriesBL = productCategoriesBL;
+            _productsBL = productsBL;
+        }
+
+        public ActionResult Index()
+        {
+            HomePageViewModel viewModel = new HomePageViewModel();
+
+            viewModel.RootCategories = _productCategoriesBL.GetCategories(null).Select(productCategory => new HorizontalCategoryItemViewModel
+                {
+                    ProductCategory = productCategory,
+                    IsSelected = false
+                }).ToList();
+
+            viewModel.Products = _productsBL.Get(null);
+
+            return View(viewModel);
+        }
+
+        public ActionResult ChangeCategory(int categoryId)
+        {
+            HomePageViewModel viewModel = new HomePageViewModel();
+            viewModel.RootCategories = _productCategoriesBL.GetCategories(null).Select(productCategory => new HorizontalCategoryItemViewModel
+                {
+                    ProductCategory = productCategory,
+                    IsSelected = productCategory.Id == categoryId
+                }).ToList();
+
+            viewModel.SelectedCategory = viewModel.RootCategories.Single(productCategory => productCategory.ProductCategory.Id == categoryId).ProductCategory;
+            viewModel.Products = _productsBL.Get(new ProductsFilter
+                {
+                    ParentId = categoryId
+                });
+
+            return View("Index", viewModel);
+        }
+
+        public ActionResult Details(int productid)
+        {
+            return View();
+        }
+    }
+}
