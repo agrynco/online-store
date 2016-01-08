92f2635c2fed333e9c0481cbf4044af87ba9cfc2
agrynco <agrynco@gmail.com>
2016-01-08 20:00:36 +0200

    OS-147 Розширити class ApplicationUser

--- Sources/OS.Business.Domain/ApplicationUser.cs
+++ Sources/OS.Business.Domain/ApplicationUser.cs
@@ -4,5 +4,8 @@ namespace OS.Business.Domain
 {
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
     }
 }
