5ffaa0ee29de105319254ef4aa6f4fa8456e92f2
Anatolii Grynchuk <agrynco@gmail.com>
2016-01-22 08:31:53 +0200

    OS-66 Розробити шаблон користувацького інтерфейсу для адміністративної частини

--- Sources/OS.Web/Controllers/Administration/BaseAdminController.cs
+++ Sources/OS.Web/Controllers/Administration/BaseAdminController.cs
@@ -3,6 +3,7 @@
 namespace OS.Web.Controllers.Administration
 {
     [Authorize(Roles = "admin" )]
+    [RoutePrefix("administration")]
     public class BaseAdminController : Controller
     {
     }
---
+++ Sources/OS.Web/Controllers/Administration/ProductionController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace OS.Web.Controllers.Administration
+{
+    public class ProductionController : BaseAdminController
+    {
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
