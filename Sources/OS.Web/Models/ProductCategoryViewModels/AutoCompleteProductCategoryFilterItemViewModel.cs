124ccf4c21febae08f398153c9543b960a13a4d6
Anatolii Grynchuk <agrynco@gmail.com>
2016-01-30 22:14:29 +0200

    OS-86 CSC error CS2001: Source file 'R:\BuildAgent\work\cfbd625a4b6d4d20\Sources\OS.Web\Models\ProductCategoryViewModels\AutoCompleteProductCategoryFilterItemViewModel.cs' could not be found.

---
+++ Sources/OS.Web/Models/ProductCategoryViewModels/AutoCompleteProductCategoryFilterItemViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OS.Web.Models.ProductCategoryViewModels
+{
+    public class AutoCompleteProductCategoryFilterItemViewModel
+    {
+        public int? Id { get; set; }
+
+        [Display(Name = "Категорія")]
+        public string Name { get; set; }
+    }
+}
