805287f3b40d4614aef289c4d7cbd5db88d38516
Anatolii Grynchuk <agrynco@gmail.com>
2016-02-24 10:37:46 +0200

    OS-171

--- Tools/prepare_dev_env.bat
+++ Tools/prepare_dev_env.bat
@@ -1,4 +1,4 @@
-SET domainName=dev.online-store.grynco.com.ua
+SET domainName=dev.allvent.com.ua
 SET currentDir=%CD%
 
 CD "%currentDir%\..\Sources\OS.Web"
@@ -8,10 +8,10 @@ CD "%currentDir%"
 
 CALL setup_site.bat %domainName% %sitePath%
 
-REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\OnlineStore\ConnectionStrings /v OnlineStore /t REG_SZ /d "Data Source=OnlineStoreDbSrv;Initial Catalog=DevOnlineStore;Persist Security Info=True;User ID=sa;Password=sa;" /f
-REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\OnlineStore\ConnectionStrings /v OnlineStore /t REG_SZ /d "Data Source=OnlineStoreDbSrv;Initial Catalog=DevOnlineStore;Persist Security Info=True;User ID=sa;Password=sa;" /f
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\allvent.com.ua\ConnectionStrings /v OnlineStore /t REG_SZ /d "Data Source=dev.sql.allvent.com.ua;Initial Catalog=dev.allvent.com.ua;Persist Security Info=True;User ID=sa;Password=sa;" /f
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\allvent.com.ua\ConnectionStrings /v OnlineStore /t REG_SZ /d "Data Source=dev.sql.allvent.com.ua;Initial Catalog=dev.allvent.com.ua;Persist Security Info=True;User ID=sa;Password=sa;" /f
 
-REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\OnlineStore\AppSettings /v ApplicationEnvironment /t REG_SZ /d "Development" /f
-REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\OnlineStore\AppSettings /v ApplicationEnvironment /t REG_SZ /d "Development" /f
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\allvent.com.ua\AppSettings /v ApplicationEnvironment /t REG_SZ /d "Development" /f
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\allvent.com.ua\AppSettings /v ApplicationEnvironment /t REG_SZ /d "Development" /f
 
 CALL _setup_unit_tests.bat
