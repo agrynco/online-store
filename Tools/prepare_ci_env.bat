70f5b6eb7e38e07a07a35219f1050d99c8db9fd9
Anatolii Grynchuk <agrynco@gmail.com>
2016-02-24 10:45:14 +0200

    OS-171

--- Tools/prepare_ci_env.bat
+++ Tools/prepare_ci_env.bat
@@ -1,10 +1,8 @@
 @SET DbUpdatesApplierFolder=%CD%\DbUpdatesApplier
 @SET DbUpdatesApplier="%DbUpdatesApplierFolder%\CI_Tests_DbUpdateApplier.Console.exe"
 
-REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\OnlineStore.Tests\AppSettings /v dbUpdatesApplierExeName /t REG_SZ /d %DbUpdatesApplier% /f
-REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\OnlineStore.Tests\AppSettings /v dbUpdatesApplierExeName /t REG_SZ /d %DbUpdatesApplier% /f
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\allvent.com.ua.tests\AppSettings /v dbUpdatesApplierExeName /t REG_SZ /d %DbUpdatesApplier% /f
 REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\OnlineStore.Tests\AppSettings /v dbUpdatesApplierExeName /t REG_SZ /d %DbUpdatesApplier% /f
 
-
-REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\OnlineStore.Tests\ConnectionStrings /v OnlineStore /t REG_SZ /d "Data Source=OnlineStoreDbSrv;Initial Catalog=CiOnlineStore_Tests;Persist Security Info=True;User ID=sa;Password=sa;" /f
-REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\OnlineStore.Tests\ConnectionStrings /v OnlineStore /t REG_SZ /d "Data Source=OnlineStoreDbSrv;Initial Catalog=CiOnlineStore_Tests;Persist Security Info=True;User ID=sa;Password=sa;" /f
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\allvent.com.ua.tests\ConnectionStrings /v OnlineStore /t REG_SZ /d "Data Source=ci-sql.allvent.com.ua;Initial Catalog=ci-allvent.com.ua_tests;Persist Security Info=True;User ID=sa;Password=sa;" /f
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\allvent.com.ua.tests\ConnectionStrings /v OnlineStore /t REG_SZ /d "Data Source=ci-sql.allvent.com.ua;Initial Catalog=ci-allvent.com.ua_tests;Persist Security Info=True;User ID=sa;Password=sa;" /f
