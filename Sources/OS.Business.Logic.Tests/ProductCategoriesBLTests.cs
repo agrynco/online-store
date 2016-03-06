d6be2e86e651851edb4e4c7b7f060a745d61f16d
Anatolii Grynchuk <agrynco@gmail.com>
2016-03-06 08:09:54 +0200

    OS-187 Complete unit test

--- Sources/OS.Business.Logic.Tests/ProductCategoriesBLTests.cs
+++ Sources/OS.Business.Logic.Tests/ProductCategoriesBLTests.cs
@@ -30,16 +30,22 @@ namespace OS.Business.Logic.Tests
             //Arrange
             ResetDataBase();
             ProductCategoriesBL target = DI.Resolve<ProductCategoriesBL>();
+            ProductsBL productsBl = DI.Resolve<ProductsBL>();
 
             //Act
-            target.SetPublish(37, false);
+            target.SetPublish(65, false);
 
             //Asserts
-            Assert.That(!target.GetById(37).Publish);
-            Assert.That(!target.GetById(38).Publish);
-            Assert.That(!target.GetById(39).Publish);
-            Assert.That(!target.GetById(40).Publish);
-            Assert.That(!target.GetById(41).Publish);
+            Assert.That(!target.GetById(65).Publish);
+            Assert.That(!productsBl.GetById(86).Publish);
+            Assert.That(!productsBl.GetById(87).Publish);
+            Assert.That(!productsBl.GetById(88).Publish);
+            Assert.That(!productsBl.GetById(89).Publish);
+            Assert.That(!productsBl.GetById(90).Publish);
+            Assert.That(!productsBl.GetById(91).Publish);
+            Assert.That(!productsBl.GetById(92).Publish);
+            Assert.That(!productsBl.GetById(93).Publish);
+            Assert.That(!productsBl.GetById(95).Publish);
         }
     }
 }
