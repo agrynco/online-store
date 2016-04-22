2029f45e9c02fd41657fc2bb72a43a7e2062bed2
agrynco <agrynco@gmail.com>
2016-04-22 07:20:57 +0300

    OS-118 (In general fixed but need some code improvemements)

--- Sources/OS.DAL.Abstract/IContactInfosRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Security.Cryptography.X509Certificates;
-using OS.Business.Domain;
-
-namespace OS.DAL.Abstract
-{
-    public interface IContactInfosRepository : IOnlineStoreRepository<ContactInfo>
-    {
-    }
-}
---
+++ Sources/OS.Web/HtmlAboutNodeProvider.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using MvcSiteMapProvider;
+using OS.Business.Domain;
+
+namespace OS.Web
+{
+    public class HtmlAboutNodeProvider : DynamicNodeProviderBase
+    {
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode node)
+        {
+            List<DynamicNode> nodes = new List<DynamicNode>();
+
+            DynamicNode dynamicNode = new DynamicNode
+                {
+                    ParentKey = "Online Store",
+                    Controller = "HtmlContents",
+                    Action = "Index"
+                };
+            dynamicNode.RouteValues.Add("htmlContentCode", HtmlContentCode.About);
+            nodes.Add(dynamicNode);
+
+            return nodes;
+        }
+    }
+    public class HtmlContactsNodeProvider : DynamicNodeProviderBase
+    {
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode node)
+        {
+            List<DynamicNode> nodes = new List<DynamicNode>();
+
+            DynamicNode dynamicNode = new DynamicNode
+            {
+                ParentKey = "Online Store",
+                Controller = "HtmlContents",
+                Action = "Index"
+            };
+            dynamicNode.RouteValues.Add("htmlContentCode", HtmlContentCode.Contacts);
+            nodes.Add(dynamicNode);
+
+            return nodes;
+        }
+    }
+
+    public class HtmlContactsEditNodeProvider : DynamicNodeProviderBase
+    {
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode node)
+        {
+            List<DynamicNode> nodes = new List<DynamicNode>();
+
+            DynamicNode dynamicNode = new DynamicNode
+            {
+                ParentKey = "Online Store",
+                Controller = "HtmlContents",
+                Action = "Edit"
+            };
+            dynamicNode.RouteValues.Add("htmlContentCode", HtmlContentCode.Contacts);
+            nodes.Add(dynamicNode);
+
+            return nodes;
+        }
+    }
+
+    public class HtmlAboutEditNodeProvider : DynamicNodeProviderBase
+    {
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode node)
+        {
+            List<DynamicNode> nodes = new List<DynamicNode>();
+
+            DynamicNode dynamicNode = new DynamicNode
+            {
+                ParentKey = "Online Store",
+                Controller = "HtmlContents",
+                Action = "Edit"
+            };
+            dynamicNode.RouteValues.Add("htmlContentCode", HtmlContentCode.About);
+            nodes.Add(dynamicNode);
+
+            return nodes;
+        }
+    }
+}
