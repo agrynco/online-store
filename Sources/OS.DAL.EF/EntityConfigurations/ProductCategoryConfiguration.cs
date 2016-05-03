68eecced09ad86a583e43f509da7765e82fe62e9
agrynco <agrynco@gmail.com>
2016-05-03 11:55:00 +0300

    OS-279 Added Order to the ProductCategory and updated ProductCategories with default order values

--- Sources/OS.DAL.EF/EntityConfigurations/ProductCategoryConfiguration.cs
+++ Sources/OS.DAL.EF/EntityConfigurations/ProductCategoryConfiguration.cs
@@ -10,18 +10,30 @@ namespace OS.DAL.EF.EntityConfigurations
         public ProductCategoryConfiguration()
         {
             const string UK_PRODUCTCATEGORY_PARENT_ID_NAME = "UK_PRODUCTCATEGORY_PARENT_ID_NAME";
+            const string UK_PRODUCTCATEGORY_PARENT_ID_ORDER = "UK_PRODUCTCATEGORY_PARENT_ID_ORDER";
             Property(p => p.ParentId).HasColumnAnnotation(IndexAnnotation.AnnotationName,
-                new IndexAnnotation(new IndexAttribute(UK_PRODUCTCATEGORY_PARENT_ID_NAME)
-                {
-                    IsUnique = true,
-                    Order = 1
-                }));
+                new IndexAnnotation(new[]
+                    {
+                        new IndexAttribute(UK_PRODUCTCATEGORY_PARENT_ID_NAME, 1)
+                            {
+                                IsUnique = true
+                            },
+                        new IndexAttribute(UK_PRODUCTCATEGORY_PARENT_ID_ORDER, 1)
+                            {
+                                IsUnique = true
+                            }
+                    }));
 
             Property(p => p.Name).IsRequired().HasMaxLength(255).HasColumnAnnotation(IndexAnnotation.AnnotationName,
-                new IndexAnnotation(new IndexAttribute(UK_PRODUCTCATEGORY_PARENT_ID_NAME)
+                new IndexAnnotation(new IndexAttribute(UK_PRODUCTCATEGORY_PARENT_ID_NAME, 2)
+                {
+                    IsUnique = true
+                }));
+
+            Property(p => p.Order).IsRequired().HasColumnAnnotation(IndexAnnotation.AnnotationName,
+                new IndexAnnotation(new IndexAttribute(UK_PRODUCTCATEGORY_PARENT_ID_ORDER, 2)
                 {
                     IsUnique = true,
-                    Order = 2
                 }));
         }
     }
