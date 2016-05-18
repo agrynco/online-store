292bcef3800b71a076bca102c4b216e9c21c6870
agrynco <agrynco@gmail.com>
2016-05-18 19:34:21 +0300

    OS-276: (Fixed) Кількість товарів на сторінці

--- Sources/OS.Web/HtmlHelperExtensions.cs
+++ Sources/OS.Web/HtmlHelperExtensions.cs
@@ -9,9 +9,16 @@ namespace OS.Web
     public static class HtmlHelperExtensions
     {
         public static MvcHtmlString Pager(this HtmlHelper htmlHelper, PaginationFilterViewModel paginationFilterViewModel,
-            Func<int, MvcHtmlString> pageItemActionLinkFunc)
+            Func<int, MvcHtmlString> pageItemActionLinkFunc,
+            Func<MvcHtmlString> prevActionLinkFunc,
+            Func<MvcHtmlString> nextActionLinkFunc)
         {
             string htmlTemplate = ResourceReader.ReadAsString(typeof (HtmlHelperExtensions), "OS.Web.HtmlTemplates.paginationTemplate.html");
+
+            htmlTemplate = htmlTemplate.Replace("{{prev}}", prevActionLinkFunc != null ? htmlHelper.Raw(prevActionLinkFunc()).ToString() : "");
+
+            htmlTemplate = htmlTemplate.Replace("{{next}}", nextActionLinkFunc != null ? htmlHelper.Raw(nextActionLinkFunc()).ToString() : "");
+
             StringBuilder itemsStringBuilder = new StringBuilder();
             for (int i = 0; i < paginationFilterViewModel.GetPagesCount(); i++)
             {
