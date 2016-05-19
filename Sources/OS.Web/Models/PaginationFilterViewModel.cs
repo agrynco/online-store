6e41149612ee9a0cfba7891ddea24449cfac805c
agrynco <agrynco@gmail.com>
2016-05-19 10:14:39 +0300

    OS-310 (Fixed) Administration->Products - Application Error

--- Sources/OS.Web/Models/PaginationFilterViewModel.cs
+++ Sources/OS.Web/Models/PaginationFilterViewModel.cs
@@ -6,17 +6,17 @@ namespace OS.Web.Models
     {
         public PaginationFilterViewModel()
         {
-            PageNumber = 0;
+            PageNumber = 1;
             PageSize = int.MaxValue;
         }
 
-        public int GetPagesCount()
-        {
-            return (int)Math.Ceiling((double)TotalRecords / PageSize);
-        }
-
-        public int PageNumber { get; set; } 
+        public int PageNumber { get; set; }
         public int PageSize { get; set; }
         public int TotalRecords { get; set; }
+
+        public int GetPagesCount()
+        {
+            return (int) Math.Ceiling((double) TotalRecords / PageSize);
+        }
     }
 }
