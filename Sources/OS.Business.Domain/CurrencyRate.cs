96139afcb1577bc1e3ba9ebdd8c51d9768fd6048
agrynco <agrynco@gmail.com>
2016-05-20 08:10:49 +0300

    OS-306 Prepared DB schems and data

---
+++ Sources/OS.Business.Domain/CurrencyRate.cs
@@ -0,0 +1,9 @@
+﻿namespace OS.Business.Domain
+{
+    public class CurrencyRate : IdentityEntity
+    {
+        public virtual Currency Currency { get; set; }
+        public int CurrencyId { get; set; }
+        public decimal Rate { get; set; }
+    }
+}
