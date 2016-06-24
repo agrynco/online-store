6d1b38dbe6cc65d69067bf60fa95c29511d00a8b
Anatolii Grynchuk <agrynco@gmail.com>
2016-06-24 16:47:39 +0300

    OS-326 Reinstalled TinyMCE through nuget packages manager

--- Sources/OS.Web/Scripts/application/utils.js
+++ Sources/OS.Web/Scripts/application/utils.js
@@ -1,18 +1,23 @@
 function initTinyMCE(selector)
 {
     tinyMCE.init(
-        {
-            selector: selector,
-            entity_encoding: "raw",
-            plugins: [
-                "autoresize",
-                "advlist autolink lists link image charmap print preview anchor",
-                "searchreplace visualblocks code fullscreen",
-                "insertdatetime media table contextmenu paste code"
-            ],
-            toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-            autoresize_bottom_margin: 0,
-            entity_encoding: "raw",
-            language: 'uk_UA'
-        });
+    {
+        selector: selector,
+        entity_encoding: "raw",
+        plugins: [
+            "autoresize",
+            "advlist autolink lists link image imagetools charmap print preview anchor",
+            "searchreplace visualblocks code fullscreen",
+            "insertdatetime media table contextmenu paste code",
+            "textcolor"
+        ],
+        toolbar:
+            'insertfile undo redo | styleselect | forecolor backcolor | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+        autoresize_bottom_margin: 0,
+        image_list: [
+            { title: 'Dog', value: 'http://dev.online-store.grynco.com.ua/productphotos/114' },
+            { title: 'Cat', value: 'mycat.gif' }
+        ]
+        //language: 'uk_UA'
+    });
 }
